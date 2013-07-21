bfd18f28aaaf68c8059dcbbcaf5f016d909d81d2
Scott Stanfield <scott@vertigo.com>
2013-07-21 14:28:28 -0700

    another name change

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello from Powerful Hollows!');
+  response.send('Hello from Kickable Kangaroo!');
 });
 
 var port = process.env.PORT || 5000;
