e2fab6bb8f5f6fa3a1226e11421f59c6df5e5f7f
Scott Stanfield <scott@vertigo.com>
2013-07-21 15:55:33 -0700

    Request for root reads from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    fs.readFile('index.html', function(err, data) {
+        if (err) throw err;
+        response.set('Content-Type', 'text/html');
+        response.send(data);
+    });
 });
 
 var port = process.env.PORT || 5000;
